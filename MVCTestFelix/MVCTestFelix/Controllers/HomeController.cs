951d06f77f16e5d49a6c15faafb383348eb21b0c
felix <felix@stebra.se>
2015-10-06 14:43:42 +0200

    commited old revetr (index())

--- MVCTestFelix/MVCTestFelix/Controllers/HomeController.cs
+++ MVCTestFelix/MVCTestFelix/Controllers/HomeController.cs
@@ -10,7 +10,19 @@ namespace MVCTestFelix.Controllers
     {
         public string Index()
         {
-            return "hello there i removed all code whoops!";
+            int high = 56;
+            int low = 4;
+            if (high > low) {
+                string message = "high is bigger than low";
+            }
+            int t1 = 54;
+            int t2 = 123;
+            if (t1 < t2)
+            {
+
+            }
+            string nu = "kom igen funka";
+            return "Hello world";
         }
     }
 }
