c0397af0fee1eceb598f591e4981e93acd863c4c
felix <felix@stebra.se>
2015-10-06 11:13:28 +0200

    Added Hello owlrd

---
+++ MVCTestFelix/MVCTestFelix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTestFelix.Startup))]
+namespace MVCTestFelix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
