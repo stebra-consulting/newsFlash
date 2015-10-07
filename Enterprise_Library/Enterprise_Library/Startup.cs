46ff9732c661f1edd8b30e6b34678322b9165f23
felix <felix@stebra.se>
2015-10-07 12:21:30 +0200

    First push by Felix, Solution with Enterprise Lib

---
+++ Enterprise_Library/Enterprise_Library/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Enterprise_Library.Startup))]
+namespace Enterprise_Library
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
