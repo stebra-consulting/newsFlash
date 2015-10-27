6e4fd82032c3e126b934bc504cd4a99dee578614
felix <felix@stebra.se>
2015-10-27 16:06:38 +0100

    MRP: Added AzureTableManager.cs Utilityclass for write/read access to Azure Table. Added Connection String to Web.Config. Listitems are saved in index(), loaded in About().

--- MRPNewsFlash/MRPNewsFlashWeb/Controllers/HomeController.cs
+++ MRPNewsFlash/MRPNewsFlashWeb/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MRPNewsFlashWeb.Models;
 
 namespace MRPNewsFlashWeb.Controllers
 {
@@ -40,6 +41,12 @@ namespace MRPNewsFlashWeb.Controllers
                     clientContext.Load(items);
                     clientContext.ExecuteQuery();
 
+                    //Create a new Azure Table for this app, uses connection string from Web.Config
+                    AzureTableManager.SelectTable();
+
+                    //Save the ListItems to AzureTable as StebraEntities
+                    AzureTableManager.SaveNews(items);
+
                     return View(); //check items
                 }
             }
@@ -49,9 +56,9 @@ namespace MRPNewsFlashWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IEnumerable<StebraEntity> news = AzureTableManager.LoadAllNews();
 
-            return View();
+            return View(news);
         }
 
         public ActionResult Contact()
