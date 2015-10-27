62fd17d9742f7e266a4691024247ea6ba96e3364
felix <felix@stebra.se>
2015-10-27 15:57:03 +0100

    MRP: installed Azure.Storage + Cloud.Configuration.Manager NuGet Packages. Added (StebraEntity.cs)-Model (Not used yet). Increased Permission to Web Read (Faster Dev)

---
+++ MRPNewsFlash/MRPNewsFlashWeb/Models/StebraEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace MRPNewsFlashWeb.Models
+{
+    public class StebraEntity : TableEntity
+    {
+        public StebraEntity()
+        { }
+        public StebraEntity(string StebraType, string NewsEntry,
+            string NewsDescription, string NewsArticle, string NewsDate, string NewsBody)
+        {
+            //hold properties that mirrors listitem-columns
+            this.PartitionKey = StebraType;
+            this.RowKey = NewsEntry;
+            this.Title = NewsEntry;
+            this.Description = NewsDescription;
+            this.Article = NewsArticle;
+            this.Date = NewsDate;
+            this.Body = NewsBody;
+        }
+
+        public string Description { get; set; }
+        public string Article { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Body { get; set; }
+    }
+}
