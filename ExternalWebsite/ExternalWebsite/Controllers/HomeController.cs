effa9e4b7c3e4bff7ddc362af7124c6a7f77d422
felix <felix@stebra.se>
2015-11-13 10:14:55 +0100

    Initial Commit, Added, AzureManager and StebraEntities, loadnews in Home/Index

---
+++ ExternalWebsite/ExternalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExternalWebsite.Models;
+
+namespace ExternalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            //List to hold news
+            List<StebraEntity> freshNews = new List<StebraEntity>();
+
+            //Get news from AzureTable
+            freshNews = AzureManager.LoadNews();
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
