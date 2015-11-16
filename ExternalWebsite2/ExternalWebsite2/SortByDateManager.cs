6f70425355ebda24ded4e601e5e941cf55680414
felix <felix@stebra.se>
2015-11-16 14:27:16 +0100

    Updated SortByDateManager +1Method

--- ExternalWebsite2/ExternalWebsite2/SortByDateManager.cs
+++ ExternalWebsite2/ExternalWebsite2/SortByDateManager.cs
@@ -16,6 +16,36 @@ namespace ExternalWebsite2
             return newsToSort;
 
         }
+        public static List<StebraEntity> NewsYoungerThanMonths(List<StebraEntity> newsToSort, int months) //specify how fresh the news must be in measurements of months
+        {
+            months = 1;
+
+            //today
+            string yyyymmdd = DateTime.Now.ToString("yyyy-MM-dd");
+
+            //todays Month
+            string mmToday = yyyymmdd.Split('-')[1];
+
+            //Last Month                          //- months, in order words, subtract 1 month from time. if month is 1
+            string mmExpired = (int.Parse(mmToday) - months).ToString();
+
+            //Date Last Month
+            string expired = yyyymmdd.Replace(mmToday, mmExpired);
+
+            //integer (Date One Month Ago)
+            int intExpired = int.Parse(expired.Replace("-", ""));
+
+
+            List<StebraEntity> listType = new List<StebraEntity>();
+
+            IEnumerable<StebraEntity> iEnumType = listType;
+
+            iEnumType = (from o in newsToSort where o.IntDate >= intExpired select o);
+
+            listType = iEnumType.ToList();
+
+            return listType;
+        }
 
 
         public static List<StebraEntity> ByMonth(List<StebraEntity> newsToSort)
